42937fc78801e0684ce465432c1bca7e338e8c54
Yongzhi Xu <785931268@qq.com>
2019-04-15 12:09:53 +0800

    unit1

---
+++ Exception_try/src/Unit1/AdditionProblem.java
@@ -0,0 +1,20 @@
+package Unit1;
+
+public class AdditionProblem {
+	private int x, y, answer;
+
+	public AdditionProblem() {
+		x = (int) (10 + 40 * Math.random());
+		y = (int) (30 * Math.random());
+		answer = x + y;
+	}
+
+	public String getProblem() {
+		return "Compute the sum: " + x + " + " + y;
+	}
+
+	public int getAnswer() {
+		return answer;
+	}
+
+}
---
+++ Exception_try/src/Unit2/Main.java
@@ -0,0 +1,9 @@
+package Unit2;
+
+import Unit1.TextIO;
+public class Main {
+
+	public static void main() {
+		TextIO.eof();
+	}
+}
