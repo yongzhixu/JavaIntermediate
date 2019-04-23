c3654b522b5ebcc8cad4c3963df4f122fe9586fe
Yongzhi Xu <785931268@qq.com>
2019-04-23 18:46:54 +0800

    binary search tree

---
+++ maven_1/src/main/java/maven_1/maven_1/App.java
@@ -0,0 +1,17 @@
+package maven_1.maven_1;
+
+/**
+ * Hello world!
+ *
+ */
+/**
+ * @author Asus
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
